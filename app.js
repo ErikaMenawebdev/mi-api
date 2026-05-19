17c59f42f7c55b0aa578f7f35e118980eceffbb4
Erika Mena <MENAPA444@GMAIL.COM>
2026-05-19 15:15:14 -0500

    Agrega CORS y middleware global de errores

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const usuariosRoutes = require("./routes/usuarios");
 const conectarDB = require("./config/db");
 const express = require("express");
+const cors = require("cors");
 const tareasRoutes = require("./routes/tareas");
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(tareasRoutes);
 app.use(usuariosRoutes);
 
